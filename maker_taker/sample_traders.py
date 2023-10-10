9be02283e8e0f47a239a4267ecad1e8a2ac797fc
Jun Heng Lor <jl850@duke.edu>
2023-10-09 22:02:56 -0400

    updated"

--- maker_taker/sample_traders.py
+++ maker_taker/sample_traders.py
@@ -2,6 +2,32 @@ from maker_taker_base import Taker, Maker, Side, Market, test
 import random
 from typing import Type
 
+class Rafael_and_Eduardo_Maker(Maker):
+    def __init__(self) -> None:
+        self.fair = 13.5
+
+    def get_market(self, round: int) -> Market():
+        if self.fair == 13.5:
+            return Market(self.fair - 4.5, 1, self.fair + 4.5, 1)
+        if self.fair == 90:
+            return Market(self.fair - 5, 1, self.fair + 5, 1)
+        if self.fair == 55:
+            return Market(self.fair - 5, 1, self.fair + 5, 1)
+        return Market(self.fair - 5, 1000, self.fair + 5, 1000)
+
+    def update_trade(self, side: Type[Side]) -> None:
+        if side == Side.sell and self.fair == 13.5:
+            self.fair = 7
+        elif side == Side.sell and self.fair == 7:
+            self.fair = 5
+        elif side == Side.sell and self.fair == 90:
+            self.fair = 55
+        elif side == Side.buy and self.fair == 13.5:
+            self.fair = 90
+        elif side == Side.buy and self.fair == 90:
+            self.fair = 95
+        elif side == Side.buy and self.fair == 7:
+            self.fair = 10
 
 class RandomTaker(Taker):
     def do_trade(self, market: Type[Market], round: int) -> Type[Side]:
